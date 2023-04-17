e42edcdb4305c6d09dfbc6a05b772494589e1be9
Shreeyash Balkrishna Bankar <130852464+Shreeyash-Bankar@users.noreply.github.com>
2023-04-17 17:18:50 +0530

    Add files via upload

---
+++ js/commands.js
@@ -0,0 +1,82 @@
+
+var twitter = "https://twitter.com/ShreeyashBankar";
+var linkedin = "https://www.linkedin.com/in/shreeyash-bankar-56ab53272/";
+var instagram = "https://www.instagram.com/shreeyashbankar025/";
+var github = "https://github.com/Shreeyash-Bankar";
+var sudo = "https://www.youtube.com/watch?v=dQw4w9WgXcQ?autoplay=1";
+var email = "shreeyashbanker17@gmail.com";
+
+aboutme = [
+  "<br>",
+  "<span class='underline'>Hey, I'm shreeyash!👋</span>",
+  "<br>",
+  "<li>🔭 I’m currently a 3rd year undergrad, pursuing BCs.</li>",
+  "<li>🌱 My fields of intetest are</li>",
+  `<pre class="whitespace-pre-wrap">
+        - Fullstack Development
+        - UI/UX Designing</pre>`,
+  `<li><pre class="whitespace-pre-wrap">👯 I would love to work with anyone who wants to build a 
+   professional website for their business or even a personal 
+   website to showcase their portfolio.
+   You can learn more about the projects that I have built using 
+   the project command.
+   You can browse around my website to find out more about 
+   me.
+   *Try using a social command.`,
+  `<li><pre class="whitespace-pre-wrap">👨‍💻 Check out my github profile to view my projects
+   *Use the github command to take a look at my GitHub profile.`,
+  `<li>📫 Contact me at <a href="https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcRzDDPFDTbvhjVpvjpNxLZPjFdzKFcmqZGNHclhwtJKhdhrBqGNVGBzlPvFfwxvksdTcqHXs">shreeyashbanker17@gmail.com</a>`,
+  "<br>",
+];
+
+social = [
+  "<br>",
+
+  'twitter        <a href="' +
+    twitter +
+    '" target="blank">twitter.com/shreeyashbankar' +
+    "</a>",
+  'linkedin       <a href="' +
+    linkedin +
+    '" target="blank">linkedin/shreeyashBankar' +
+    "</a>",
+  'instagram      <a href="' +
+    instagram +
+    '" target="blank">instagram/shreeyasbanker025' +
+    "</a>",
+  'github         <a href="' +
+    github +
+    '" target="blank">github/Shreeyash-Bankar' +
+    "</a>",
+  
+  "<br>",
+];
+
+projects = [
+  "<br>",
+  `still working on them :D`,
+  "<br>",
+];
+
+help = [
+  `<br><pre class="whitespace-pre-wrap">
+  <span class="command">aboutme</span>    
+   <span class="command">↳</span>&nbsp;Diplays Who I am?
+  <span class="command">social</span>     
+   <span class="command">↳</span>&nbsp;Lists social networks.
+  <span class="command">projects</span>   
+   <span class="command">↳</span>&nbsp;View coding projects.
+  <span class="command">email</span>      
+   <span class="command">↳</span>&nbsp;To send me a email.
+  <span class="command">history</span>    
+   <span class="command">↳</span>&nbsp;View command history.
+  <span class="command">help</span>       
+   <span class="command">↳</span>&nbsp;Displays this help msg.
+  <span class="command">sudo</span>       
+   <span class="command">↳</span>&nbsp;Try it out for yourself.
+  <span class="command">clear</span>
+   <span class="command">↳</span>&nbsp;Clear terminal.</pre><br>`,
+];
+banner = [
+  `<div class="pt-2"><span class="text-[#7d82d7db] ">Welcome to my portfolio! — Type <span class="command text-[#75e1e7]">help</span> for a list of supported commands.</span></div>`,
+];
